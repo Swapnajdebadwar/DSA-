7bc5801464e4cec4afdd4940872c4134ebf43ea0
swapnajdebadwar <swapnajdebadwar3@gmail.com>
2026-02-26 17:05:54 +0530

    updated

--- Takinginput.java
+++ Takinginput.java
@@ -5,7 +5,7 @@ public class Takinginput {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int c = a+b;
+        int c = a+b; 
         System.out.println("Addition is "+c);
     }
     
---
+++ operators.java
@@ -0,0 +1,19 @@
+public class operators {
+    public static void main(String args[]){
+        //Arithemetic operators 
+        int a = 10;
+        int b = 20;
+        int Addition  = a+b;
+        int substraction = a-b;
+        int multiplication = a*b;
+        int division = a/b;
+        int modules = a%b;
+        System.out.println("Addition of two number is "+Addition);
+        System.out.println("Substraction of two number is "+substraction);
+        System.out.println("Multiplication of two number is "+multiplication);
+        System.out.println("Division of two number is "+division);
+        System.out.println("modules of two number is "+modules);
+
+        //Assignment operato
+    }
+}
