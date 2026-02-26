114866e8a9c15c547b2730f236f03d7bffc60c7d
swapnajdebadwar <swapnajdebadwar3@gmail.com>
2026-02-26 12:10:59 +0530

    updated

--- basics.java
+++ basics.java
@@ -1,5 +1,8 @@
 public class basics {
     public static void main(String[] args) {
-        System.out.println("I love India");
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println("The addition of a and b i s"+c);
     }
 }
