378e58114c29a0b3dfb1a828bcd5e9b05fe59129
swapnajdebadwar <swapnajdebadwar3@gmail.com>
2026-02-26 12:15:31 +0530

    updated

--- basics.java
+++ basics.java
@@ -1,8 +1,8 @@
 public class basics {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = a+b;
-        System.out.println("The addition of a and b i s"+c);
+        int a = 10; //variable decleration
+        int b = 20; //variable declearation
+        int c = a+b; //storing the result 
+        System.out.println("The addition of a and b i s"+c);//printing the result 
     }
 }
