67cfb9a32fe39f883da148f7f468dc3b922dca98
swapnajdebadwar <swapnajdebadwar3@gmail.com>
2026-03-02 13:10:27 +0530

    updated

---
+++ conditional_statment.java
@@ -0,0 +1,11 @@
+public class conditional_statment {
+        public static void main (String args[]){
+            int age = 18;
+            if(age>=18){
+                System.out.println("Adult:driving,oting allowed");
+            }
+            else{
+                System.out.println("not able to vote and drive ");
+            }
+        }
+}
